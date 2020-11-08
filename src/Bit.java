5e192d3e9891d4890fe60e84254d0d3da58db507
Korsaras90 <sergbirukof@gmail.com>
2020-11-08 20:40:25 +0300

    Initial commit

---
+++ src/Bit.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Bit {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        int[] mas = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            mas[i] = in.nextInt();
+        }
+
+        System.out.println();
+    }
+}
